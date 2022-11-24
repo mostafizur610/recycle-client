9dc7b4504db5ef8e17aa86f9857c5c1b0ee865f3
mostafizur610 <mostafizurrahman273@gmail.com>
2022-11-25 05:38:09 +0600

    set 404 route

--- src/Pages/shared/NavigationBar/NavigationBar.js
+++ src/Pages/shared/NavigationBar/NavigationBar.js
@@ -3,24 +3,25 @@ import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
     return (
-        <div className="navbar shadow-slate-900">
+        <div className="navbar bg-amber-100 border-secondary">
             <div className="flex-1">
-                <Link to='/' ><span className="btn btn-ghost normal-case text-2xl text-green-900">exDesktop Accessories </span> <span className='text-blue-600'> <br /> Resale Products</span></Link>
+                <Link to='/' ><span className="btn btn-ghost normal-case text-4xl text-blue-900">exDesktop Accessories </span> </Link>
+                {/* <span className='text-blue-600 pl-6'> <br /> Resale Products</span> */}
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
                     <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/blogs'>Blog</Link></li>
                     <li tabIndex={0}>
-                        <a>
-                            Parent
+                        <Link>
+                            Login
                             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
+                        </Link>
                         <ul className="p-2 bg-base-100">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
+                            <li><Link to='/userLogin'>User Login</Link></li>
+                            <li><Link to='/sellerLogin'>Seller Login</Link></li>
                         </ul>
                     </li>
-                    <li><Link to='/blogs'>Blog</Link></li>
                 </ul>
             </div>
         </div>
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom"
+import notFound from '../../assets/not found/not found.webp'
 import Main from "../../Layout/Main/Main"
 import Blogs from "../../Pages/Blogs/Blogs"
 import Home from "../../Pages/Home/Home/Home"
@@ -18,6 +19,13 @@ const router = createBrowserRouter([
             }
 
         ]
+    },
+    {
+        path: '*',
+        element: <div><img className="w-full h-96" src={notFound} alt="" />
+            <h1 className='flex justify-center text-red-900 text-5xl'>Not Found</h1><br /><h4 className='flex justify-center text-yellow-700 text-xl'>This route is not defined</h4>
+
+        </div>
     }
 ])
 
