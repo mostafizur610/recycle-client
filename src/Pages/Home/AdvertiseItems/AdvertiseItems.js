5ddc65c25ca634583c80a35670f97b2f6b7e0f51
mostafizur610 <mostafizurrahman273@gmail.com>
2023-02-07 18:17:29 +0600

    step added

--- src/Pages/Home/AdvertiseItems/AdvertiseItems.js
+++ src/Pages/Home/AdvertiseItems/AdvertiseItems.js
@@ -18,24 +18,36 @@ const AdvertiseItems = () => {
     }, []);
 
     return (
-        <div className='p-12'>
-            <h1 className='text-center text-4xl text-accent'>Most recent products Ad. For sale</h1>
-            <div className='mt-12 flex flex-wrap gap-5 border-spacing-8 text-center text-white text-3xl'>
-                {advertisedProducts && advertisedProducts.map(advertisedProduct => (
-                    advertisedProduct.products.map(product => (
-                        <>
-                            <div>
-                                <img className='h-32 w-48' src={product.p_img} alt="" />
-                                {/* <label onClick={() => clickHandle(category)} htmlFor="booking-modal" className="btn btn-primary">{product.p_name}</label> */}
-                                <Link to='/'><p className='text-blue-900 btn btn-outline'>Buy {product.p_name}</p></Link>
-                            </div>
-                        </>
-
-                    ))
-                ))
-                }
+        <div>
+            <div className='flex justify-center'>
+                <ul className="steps steps-vertical lg:steps-horizontal">
+                    <li className="step step-primary">Login</li>
+                    <li className="step step-primary">Choose Products</li>
+                    <li className="step">Give Location</li>
+                    <li className="step">Purchase</li>
+                    <li className="step">Receive Product</li>
+                </ul>
             </div>
 
+            <div className='p-12'>
+                <h1 className='text-center text-4xl text-accent'>Most recent products Ad. For sale</h1>
+                <div className='mt-12 flex flex-wrap gap-5 border-spacing-8 text-center text-white text-3xl'>
+                    {advertisedProducts && advertisedProducts.map(advertisedProduct => (
+                        advertisedProduct.products.map(product => (
+                            <>
+                                <div>
+                                    <img className='h-32 w-48' src={product.p_img} alt="" />
+                                    {/* <label onClick={() => clickHandle(category)} htmlFor="booking-modal" className="btn btn-primary">{product.p_name}</label> */}
+                                    <Link to='/'><p className='text-blue-900 btn btn-outline'>Buy {product.p_name}</p></Link>
+                                </div>
+                            </>
+
+                        ))
+                    ))
+                    }
+                </div>
+
+            </div>
         </div>
     )
 };
